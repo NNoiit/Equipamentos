10757d0c3328c7bf7890d6c1a9cae09d6163b768
Cristion A.Assis <57966693+NNoiit@users.noreply.github.com>
2022-12-19 12:57:47 -0300

    teste
    Mudança do tipo de retorno da classe deleteBicicleta(), na classe bicicletaController

--- src/main/java/com/api/equipamento/controller/BicicletaController.java
+++ src/main/java/com/api/equipamento/controller/BicicletaController.java
@@ -51,7 +51,7 @@ public class BicicletaController {
 
         mensage = service.excluirBicicleta(id);
 
-        return new ResponseEntity<Mensage>(mensage);
+        return new ResponseEntity<String>(mensage.getMensage());
     }
 
     //corrigir dps, o objetivo é alterar a ação
