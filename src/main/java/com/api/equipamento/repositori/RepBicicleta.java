501e674b9ba7cec9713dce090d2c7007384aec7f
NNoiit <Nnoitsirc@edu.unirio.br>
2023-01-18 14:10:45 -0300

    importações não utilizadas retiradas

--- src/main/java/com/api/equipamento/repositori/RepBicicleta.java
+++ src/main/java/com/api/equipamento/repositori/RepBicicleta.java
@@ -1,11 +1,9 @@
 package com.api.equipamento.repositori;
 
 import com.api.equipamento.model.Bicicleta;
-import org.hibernate.sql.Delete;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
 import java.util.UUID;
--- src/main/java/com/api/equipamento/repositori/RepTotem.java
+++ src/main/java/com/api/equipamento/repositori/RepTotem.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.UUID;
 
