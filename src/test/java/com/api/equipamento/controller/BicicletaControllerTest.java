cf51ada4323116aa79936b4baed3874eb06efef1
NNoiit <Nnoitsirc@edu.unirio.br>
2023-01-18 12:18:32 -0300

    Teste do primeniro And realizado com exito

--- src/test/java/com/api/equipamento/controller/BicicletaControllerTest.java
+++ src/test/java/com/api/equipamento/controller/BicicletaControllerTest.java
@@ -3,7 +3,6 @@ package com.api.equipamento.controller;
 import com.api.equipamento.model.Bicicleta;
 import com.api.equipamento.model.Erro;
 import com.api.equipamento.service.BicicletaService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,20 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("BicicletaControllerTest")
 public class BicicletaControllerTest {
     @Autowired
-    private BicicletaController bicicletaController;
-    @MockBean
     private MockMvc mockMvc;
 
     @MockBean
@@ -32,20 +29,20 @@ public class BicicletaControllerTest {
 
     @MockBean
     private Bicicleta bicicleta;
+
+    @MockBean
+    private BicicletaService bicicletaService;
     @Test
     void teste() throws Exception {
         this.mockMvc.perform(get("/"))
                 .andDo(print())
-                .andExpect(content().string("Bem Vindos a Nossa Bike &#9773;"));
-
-                //andExpect("Bem Vindos a Nossa Bike &#9773;");
-        //Assertions.assertEquals("Bem Vindos a Nossa Bike &#9773;", bicicletaController.teste());
+                .andExpect(status()
+                .isOk()).andExpect(content().string("Bem Vindos a Nossa Bike &#9773;"));
     }
 
     @Test
     void postBicicleta(){
-        //Mockito.when(bicicletaService.cadastrar(bicicleta)).thenReturn(bicicleta);
-        //bicicletaController.postBicicleta(bicicleta);
+        Mockito.when(bicicletaService.cadastrar(bicicleta)).thenReturn(bicicleta);
 
     }
 }
